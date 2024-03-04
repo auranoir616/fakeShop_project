80416607230c9d843833e876f75259460bc22c1b
auranoir616 <128385320+auranoir616@users.noreply.github.com>
2024-03-04 12:57:54 +0700

    as

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <div>
       <Navbar />
       <h1 className="text-3xl font-bold underline">
-      Hello world!
+      Hello world!ss
     </h1>
     <Button>Button</Button>
     </div>
